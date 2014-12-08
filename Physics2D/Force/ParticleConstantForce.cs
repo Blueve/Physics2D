67080b572edc6a788568a5f427c72570eb9ed676
Blueve <672454911@qq.com>
2014-12-08 11:08:21 +0800

    增加恒力发生器

---
+++ Physics2D/Force/ParticleConstantForce.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Physics2D.Common;
+using Physics2D.Object;
+
+namespace Physics2D.Force
+{
+    public class ParticleConstantForce : ParticleForceGenerator
+    {
+        private Vector2D force;
+
+        public ParticleConstantForce(Vector2D _force)
+        {
+            force = _force;
+        }
+
+        public override void UpdateForce(Particle particle, float duration)
+        {
+            particle.AddForce(force);
+        }
+    }
+}
