d967aa2816e4e5dd9bcb4a96001625fa2cee50f5
Blueve <672454911@qq.com>
2014-12-09 21:08:18 +0800

    增加质体绳索模型

---
+++ Physics2D/Collision/ParticleRope.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Physics2D.Common;
+using Physics2D.Object;
+
+namespace Physics2D.Collision
+{
+    public class ParticleRope : ParticleLink
+    {
+        /// <summary>
+        /// 最大长度
+        /// </summary>
+        public float maxLength;
+
+        /// <summary>
+        /// 弹性系数
+        /// </summary>
+        public float restitution;
+
+        public ParticleRope(float maxLength, float restitution, Particle pA, Particle pB)
+        {
+            this.maxLength = maxLength;
+            this.restitution = restitution;
+            PA = pA;
+            PB = pB;
+        }
+
+        public override int fillContact(List<ParticleContact> contactList, int limit)
+        {
+            float length = currentLength();
+
+            // 未超过绳索长度
+            if (length < maxLength) return 0;
+
+            Vector2D normal = (PB.Position -　PA.Position).Normalize();
+
+            ParticleContact contact = new ParticleContact
+            {
+                PA = PA,
+                PB = PB,
+                restitution = restitution,
+                contactNormal = normal,
+                penetration = length - maxLength
+            };
+
+            contactList.Add(contact);
+
+            return 1;
+        }
+    }
+}
