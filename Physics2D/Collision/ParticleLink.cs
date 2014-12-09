533718ce8373d43a99aa4fcfefba649af1a7c3df
Blueve <672454911@qq.com>
2014-12-09 21:08:03 +0800

    增加质体关联

---
+++ Physics2D/Collision/ParticleLink.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Physics2D.Common;
+using Physics2D.Force;
+using Physics2D.Object;
+
+namespace Physics2D.Collision
+{
+    public abstract class ParticleLink : ParticleContactGenerator
+    {
+        /// <summary>
+        /// 质体A
+        /// </summary>
+        public Particle PA;
+        /// <summary>
+        /// 质体B
+        /// </summary>
+        public Particle PB;
+
+        /// <summary>
+        /// 返回当前连接的长度
+        /// </summary>
+        /// <returns></returns>
+        protected float currentLength()
+        {
+            return (PA.Position - PB.Position).Length();
+        }
+    }
+}
