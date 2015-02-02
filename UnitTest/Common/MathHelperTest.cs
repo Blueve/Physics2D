b55d4fbde8e52d2b2bcfaaae2e5ee0bf3f6c2940
Blueve <672454911@qq.com>
2015-02-02 20:50:22 +0800

    增加了相交检测的单元测试

---
+++ UnitTest/Common/MathHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Physics2D.Common;
+
+namespace UnitTest.Common
+{
+    [TestClass]
+    public class MathHelperTest
+    {
+        [TestMethod]
+        public void TestIsLineIntersection()
+        {
+            Vector2D pA1 = new Vector2D(0f, 3f);
+            Vector2D pA2 = new Vector2D(6f, 3f);
+
+            Vector2D pB1 = new Vector2D(3f, 0f);
+            Vector2D pB2 = new Vector2D(3f, 6f);
+
+
+            Assert.IsTrue(MathHelper.IsLineIntersection(pA1, pA2, pB1, pB2));
+        }
+
+        [TestMethod]
+        public void TestLineIntersection()
+        {
+            Vector2D pA1 = new Vector2D(0f, 3f);
+            Vector2D pA2 = new Vector2D(6f, 3f);
+
+            Vector2D pB1 = new Vector2D(3f, 0f);
+            Vector2D pB2 = new Vector2D(3f, 6f);
+
+            var actual = MathHelper.LineIntersection(pA1, pA2, pB1, pB2);
+            Assert.AreEqual(new Vector2D(3f, 3f), actual);
+        }
+    }
+}
