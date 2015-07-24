42ee24e9124c5af68319be044ef6ca73553c5a1a
Blueve <672454911@qq.com>
2015-07-24 17:42:56 +0800

    更新项目至VS2015

--- Physics2D/Properties/AssemblyInfo.cs
+++ Physics2D/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过以下
@@ -33,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Physics2D.Tests")]
+[assembly: InternalsVisibleTo("Physics2D.Explorables")]
+
