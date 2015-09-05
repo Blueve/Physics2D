0201748dec2b098d81982acbd8a5cea0cbc711fc
Blueve <672454911@qq.com>
2015-09-05 21:00:37 +0800

    去除float标记

--- UnitTest/Common/MathHelperTest.cs
+++ UnitTest/Common/MathHelperTest.cs
@@ -11,14 +11,14 @@ namespace UnitTest.Common
         [TestMethod]
         public void TestLineIntersection()
         {
-            Vector2D pA1 = new Vector2D(0f, 3f);
-            Vector2D pA2 = new Vector2D(6f, 3f);
+            Vector2D pA1 = new Vector2D(0, 3);
+            Vector2D pA2 = new Vector2D(6, 3);
 
-            Vector2D pB1 = new Vector2D(3f, 0f);
-            Vector2D pB2 = new Vector2D(3f, 6f);
+            Vector2D pB1 = new Vector2D(3f, 0);
+            Vector2D pB2 = new Vector2D(3, 6);
 
             var actual = MathHelper.LineIntersection(pA1, pA2, pB1, pB2);
-            Assert.AreEqual(new Vector2D(3f, 3f), actual);
+            Assert.AreEqual(new Vector2D(3, 3), actual);
         }
     }
 }
--- UnitTest/Common/Vector2DTest.cs
+++ UnitTest/Common/Vector2DTest.cs
@@ -13,33 +13,33 @@ namespace UnitTest.Common
             Vector2D vector = new Vector2D();
             vector.X = vector.Y = 0;
 
-            Assert.AreEqual(vector, new Vector2D(0f, 0f));
+            Assert.AreEqual(vector, new Vector2D(0, 0));
             Assert.AreEqual(vector, new Vector2D(vector));
         }
 
         [TestMethod]
         public void TestVectorOverrideFunction()
         {
-            Vector2D vector = new Vector2D(1f, 2f);
+            Vector2D vector = new Vector2D(1, 2);
 
             // Test Equals
-            Assert.IsTrue(vector.Equals(new Vector2D(1f, 2f)));
+            Assert.IsTrue(vector.Equals(new Vector2D(1, 2)));
 
             // Test ToString
-            Assert.AreEqual("(1.23, 4.56)", new Vector2D(1.23f, 4.56f).ToString());
+            Assert.AreEqual("(1.23, 4.56)", new Vector2D(1.23, 4.56).ToString());
         }
 
         [TestMethod]
         public void TestVectorOperator()
         {
-            Vector2D left = new Vector2D(3f, 4f);
-            Vector2D right = new Vector2D(2f, 2f);
+            Vector2D left = new Vector2D(3, 4);
+            Vector2D right = new Vector2D(2, 2);
 
             // Test !=
             Assert.IsTrue(left != right);
 
             Vector2D actual = left + right;
-            Vector2D expected = new Vector2D(5f, 6f);
+            Vector2D expected = new Vector2D(5, 6);
 
             // Test ==
             Assert.IsTrue(actual == expected);
@@ -49,21 +49,21 @@ namespace UnitTest.Common
 
             // Test -
             actual = left - right;
-            expected = new Vector2D(1f, 2f);
+            expected = new Vector2D(1, 2);
             Assert.AreEqual(expected, actual);
-            Assert.AreEqual(new Vector2D(-3f, -4f), -left);
+            Assert.AreEqual(new Vector2D(-3, -4), -left);
 
             // Test *
-            Assert.AreEqual(3f * 2f + 4f * 2f, left * right);
-            Assert.AreEqual(new Vector2D(6f, 8f), left * 2f);
-            Assert.AreEqual(new Vector2D(-10f, -10f), -5f * right);
+            Assert.AreEqual(3 * 2 + 4 * 2, left * right);
+            Assert.AreEqual(new Vector2D(6, 8), left * 2);
+            Assert.AreEqual(new Vector2D(-10, -10), -5 * right);
         }
 
         [TestMethod]
         public void TestVectorMethod()
         {
-            Vector2D vect1 = new Vector2D(3f, 0f);
-            Vector2D vect2 = new Vector2D(0f, 4f);
+            Vector2D vect1 = new Vector2D(3, 0);
+            Vector2D vect2 = new Vector2D(0, 4);
 
             // Test Distance
             Assert.AreEqual(25f, Vector2D.DistanceSquared(vect1, vect2));
