644828d3dd327c2a6e6da5c9eda133b56fb5196e
Blueve <672454911@qq.com>
2015-10-09 10:31:43 +0800

    更新测试用例

--- UnitTest/Common/MathHelperTest.cs
+++ UnitTest/Common/MathHelperTest.cs
@@ -11,14 +11,21 @@ namespace UnitTest.Common
         [TestMethod]
         public void TestLineIntersection()
         {
+            // 有交点的情况
             Vector2D pA1 = new Vector2D(0, 3);
             Vector2D pA2 = new Vector2D(6, 3);
 
-            Vector2D pB1 = new Vector2D(3f, 0);
+            Vector2D pB1 = new Vector2D(3, 0);
             Vector2D pB2 = new Vector2D(3, 6);
 
             var actual = MathHelper.LineIntersection(pA1, pA2, pB1, pB2);
             Assert.AreEqual(new Vector2D(3, 3), actual);
+
+            // 无交点的情况
+            Vector2D pC1 = new Vector2D(1, 4);
+            Vector2D pC2 = new Vector2D(7, 4);
+
+            Assert.IsNull(MathHelper.LineIntersection(pA1, pA2, pC1, pC2));
         }
     }
 }
