c2db1308faff83a6626b8aac3e32f4becefd3438
Blueve <672454911@qq.com>
2015-10-19 23:40:36 +0800

    增加图元类

---
+++ Physics2D/Collision/Shapes/Shape.cs
@@ -0,0 +1,14 @@
+﻿using Physics2D.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision.Shapes
+{
+    public abstract class Shape<T>
+    {
+        public T body;
+    }
+}
---
+++ Physics2D/Collision/Shapes/Sphere.cs
@@ -0,0 +1,16 @@
+﻿using Physics2D.Common;
+using Physics2D.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision.Shapes
+{
+    public sealed class Sphere<T> : Shape<T> where T : PhysicsObject
+    {
+        public Vector2D Position;
+        public double R;
+    }
+}
