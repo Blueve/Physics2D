56b35897299f99357c39576a6ed81f0d999dd849
Blueve <672454911@qq.com>
2015-10-20 14:24:02 +0800

    增加新类型

---
+++ Physics2D/Collision/Shapes/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision.Shapes
+{
+    /// <summary>
+    /// 点形状适用于无形体的物体，通常将其作为物体的默认形状
+    /// 点状物体不参与碰撞检测
+    /// </summary>
+    public sealed class Point : Shape
+    {
+        public override ShapeType Type
+        {
+            get { return ShapeType.Point; }
+        }
+    }
+}
---
+++ Physics2D/Collision/Shapes/ShapeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision.Shapes
+{
+    public enum ShapeType
+    {
+        Point,
+        Circle,
+        Edge,
+        Box
+    }
+}
