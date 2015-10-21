81ba6bb8e9b710dbffeee97c6cf3e7d51136e1ef
Blueve <672454911@qq.com>
2015-10-21 12:55:08 +0800

    更新序号

--- Physics2D/Collision/Shapes/ShapeType.cs
+++ Physics2D/Collision/Shapes/ShapeType.cs
@@ -8,9 +8,9 @@ namespace Physics2D.Collision.Shapes
 {
     public enum ShapeType
     {
-        Point = 0,
-        Circle = 1,
-        Edge = 2,
-        Box = 3
+        Point = -1,
+        Circle = 0,
+        Edge = 1,
+        Box = 2
     }
 }
