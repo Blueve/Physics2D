6f8183b3ed98cc5ad294997deb682903b3897c09
Blueve <672454911@qq.com>
2015-10-21 11:05:15 +0800

    增加碰撞类型枚举

---
+++ Physics2D/Collision/ContactType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision
+{
+    public enum ContactType
+    {
+        CircleAndCircle,
+        CircleAndEdge,
+        CircleAndBox,
+        EdgeAndEdge,
+        EdgeAndBox,
+        BoxAndBox
+    }
+}
