4436bd7e7ca34b0a1ffa920417e0a8aec2b05bc2
Blueve <672454911@qq.com>
2015-10-22 10:29:32 +0800

    增加圆边缘的碰撞检测

--- Physics2D/Collision/ParticleCollisionDetector.cs
+++ Physics2D/Collision/ParticleCollisionDetector.cs
@@ -1,4 +1,5 @@
 ﻿using Physics2D.Collision.Shapes;
+using Physics2D.Common;
 using Physics2D.Object;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,18 @@ namespace Physics2D.Collision
 {
     public class ParticleCollisionDetector
     {
+        /// <summary>
+        /// 检测圆形与圆形间的碰撞
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="contact"></param>
+        /// <returns></returns>
         public static int CircleAndCircle(Circle A, Circle B, out ParticleContact contact)
         {
             contact = null;
+            if (((Particle)A.Body).IsTransparent || ((Particle)B.Body).IsTransparent)
+                return 0;
 
             var d = (A.Body.Position - B.Body.Position).Length();
             // 碰撞检测
@@ -29,6 +39,83 @@ namespace Physics2D.Collision
                 ContactNormal = (A.Body.Position - B.Body.Position).Normalize()
             };
             return 1;
+        }
+
+        /// <summary>
+        /// 检测圆形与边缘的碰撞
+        /// </summary>
+        /// <param name="circle"></param>
+        /// <param name="edge"></param>
+        /// <param name="contact"></param>
+        /// <returns></returns>
+        public static int CircleAndEdge(Circle circle, Edge edge, out ParticleContact contact)
+        {
+            contact = null;
+
+            var BA = edge.PointB - edge.PointA;
+            var intersectionPoint = MathHelper.LineIntersection(circle.Body.PrePosition, circle.Body.Position, edge.PointA, edge.PointB);
+
+            // 检测物体的运动路径是否发生穿越
+            if (intersectionPoint != null)
+            {
+                // 发生穿越则认为发生碰撞 将质体位置退至相交点
+                circle.Body.Position = (Vector2D)intersectionPoint;
+                var normal = BA * (circle.Body.PrePosition - edge.PointA) * BA / BA.LengthSquared();
+                normal = (circle.Body.PrePosition - edge.PointA) - normal;
+
+                contact = new ParticleContact
+                {
+                    PA = circle.Body,
+                    Restitution = circle.Body.Restitution,
+                    Penetration = circle.R,
+                    ContactNormal = normal.Normalize()
+                };
+                return 1;
+            }
+
+            // 若未发生穿越则计算
+            double n1 = (edge.PointA - circle.Body.Position) * (edge.PointA - edge.PointB);
+            double n2 = (edge.PointB - circle.Body.Position) * (edge.PointA - edge.PointB);
+
+            if (n1 * n2 <= 0)
+            {
+                // 圆心的投影在线上
+                var normal = BA * (circle.Body.Position - edge.PointA) * BA / BA.LengthSquared();
+                normal = (circle.Body.Position - edge.PointA) - normal;
+                // 线到圆心的距离
+                var d = normal.Length();
+                if (circle.R > d)
+                {
+                    contact = new ParticleContact
+                    {
+                        PA = circle.Body,
+                        Restitution = circle.Body.Restitution,
+                        Penetration = circle.R - d,
+                        ContactNormal = normal.Normalize()
+                    };
+                    return 1;
+                }
+                
+            }
+            else
+            {
+                // 圆心的投影在线外
+                var dAO = (circle.Body.Position - edge.PointA).LengthSquared();
+                var dBO = (circle.Body.Position - edge.PointB).LengthSquared();
+                if (circle.R * circle.R > Math.Min(dAO, dBO))
+                {
+                    contact = new ParticleContact
+                    {
+                        PA = circle.Body,
+                        Restitution = circle.Body.Restitution,
+                        Penetration = circle.R - Math.Sqrt(dAO < dBO ? dAO : dBO),
+                        ContactNormal = circle.Body.Position - (dAO < dBO ? edge.PointA : edge.PointB)
+                    };
+                    return 1;
+                }
+            }
+            
+            return 0;
         }
     }
 }
---
+++ Physics2D/Collision/Shapes/Edge.cs
@@ -0,0 +1,32 @@
+﻿using Physics2D.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Collision.Shapes
+{
+    public sealed class Edge : Shape
+    {
+        public Vector2D PointA;
+        public Vector2D PointB;
+
+        public Edge(Vector2D pA, Vector2D pB)
+        {
+            PointA = pA;
+            PointB = pB;
+        }
+
+        public Edge(double x1, double y1, double x2, double y2)
+        {
+            PointA = new Vector2D(x1, y1);
+            PointB = new Vector2D(x2, y2);
+        }
+
+        public override ShapeType Type
+        {
+            get { return ShapeType.Edge; }
+        }
+    }
+}
