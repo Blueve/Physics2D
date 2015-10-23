263bcfc6a701e9b67d24d56ff145ab15f0e1a547
Blueve <672454911@qq.com>
2015-10-24 01:02:40 +0800

    精简代码

--- WPFDemo/ContactDemo/ContactDemo.cs
+++ WPFDemo/ContactDemo/ContactDemo.cs
@@ -55,7 +55,7 @@ namespace WPFDemo.ContactDemo
             }
 
             // 增加重力和空气阻力
-            PhysicsWorld.CreateGlobalZone(new ParticleGravity(new Vector2D(0f, 40)));
+            PhysicsWorld.CreateGlobalZone(new ParticleGravity(new Vector2D(0, 40)));
             PhysicsWorld.CreateParticle(Vector2D.Zero, new Vector2D(1, 0), 1);
             Slot = 1 / 1000.0;
 
