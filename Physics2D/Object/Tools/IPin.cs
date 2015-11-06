16caef9ea56eba83d09ec11bccd616a1f80f905a
Blueve <672454911@qq.com>
2015-11-06 20:34:47 +0800

    增加IPin接口
    物体通过实现该接口来增加Pin功能

---
+++ Physics2D/Object/Tools/IPin.cs
@@ -0,0 +1,29 @@
+﻿using Physics2D.Common;
+using Physics2D.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Object.Tools
+{
+    interface IPin
+    {
+        /// <summary>
+        /// 把自己固定到物理世界中
+        /// 返回锚点
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        Handle Pin(World world, Vector2D position);
+
+        /// <summary>
+        /// 把自己从物理世界中解除固定
+        /// </summary>
+        /// <param name="world"></param>
+        /// <returns></returns>
+        void UnPin(World world);
+    }
+}
