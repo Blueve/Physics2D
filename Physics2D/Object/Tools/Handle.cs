aa8f304dd1a82ab6e0e720f1e399f96bc8ebd41f
Blueve <672454911@qq.com>
2015-11-06 20:34:08 +0800

    增加Handle类
    用于返回锚定位置

---
+++ Physics2D/Object/Tools/Handle.cs
@@ -0,0 +1,46 @@
+﻿using Physics2D.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Object.Tools
+{
+    public class Handle : INotifyPropertyChanged
+    {
+        private Vector2D _position;
+
+        public Vector2D Position
+        {
+            get { return _position; }
+            set
+            {
+                if(value != _position)
+                {
+                    _position = value;
+                    SetProperty(ref _position, value);
+                }
+            }
+        }
+
+        protected bool SetProperty<T>(ref T storge, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (object.Equals(storge, value)) return false;
+            storge = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            var eventHandler = PropertyChanged;
+            if (eventHandler != null)
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
