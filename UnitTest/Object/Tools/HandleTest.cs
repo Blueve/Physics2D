6bdbc4dcaa4f9f52d5dd8a5ac6ef700ed0197107
Blueve <672454911@qq.com>
2015-11-09 21:20:36 +0800

    增加新的单元测试

---
+++ UnitTest/Object/Tools/HandleTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Physics2D.Object.Tools;
+using Physics2D.Common;
+
+namespace UnitTest.Object.Tools
+{
+    [TestClass]
+    public class HandleTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Handle handle = new Handle(new Vector2D(0, 0));
+
+            Assert.IsNotNull(handle);
+            Assert.AreEqual(new Vector2D(0, 0), handle.Position);
+        }
+
+        [TestMethod]
+        public void TestPropertyChangedEvent()
+        {
+            Handle handle = new Handle(new Vector2D(0, 0));
+
+            Vector2D position = Vector2D.Zero;
+            handle.PropertyChanged += (sender, e) =>
+            {
+                position = (sender as Handle).Position;
+            };
+
+            handle.Position = new Vector2D(1, 0);
+            Assert.AreEqual(new Vector2D(1, 0), position);
+
+            handle.Position = new Vector2D(1, 0);
+            Assert.AreEqual(new Vector2D(1, 0), position);
+        }
+
+        [TestMethod]
+        public void TestRelease()
+        {
+            Handle handle = new Handle(new Vector2D(0, 0));
+
+            Vector2D position = Vector2D.Zero;
+            handle.PropertyChanged += (sender, e) =>
+            {
+                position = (sender as Handle).Position;
+            };
+            handle.Release();
+
+            handle.Position = new Vector2D(1, 0);
+            Assert.AreEqual(Vector2D.Zero, position);
+        }
+    }
+}
