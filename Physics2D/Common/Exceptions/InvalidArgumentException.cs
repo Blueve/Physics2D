c95414dd759d8e6da6fb07694d1d74e0ad4f953a
Blueve <672454911@qq.com>
2015-11-09 16:51:55 +0800

    增加了不合法参数异常类

---
+++ Physics2D/Common/Exceptions/InvalidArgumentException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics2D.Common.Exceptions
+{
+    public class InvalidArgumentException : ArgumentException
+    {
+        public InvalidArgumentException(string message, string paramName)
+            :base(message, paramName)
+        { }
+    }
+}
