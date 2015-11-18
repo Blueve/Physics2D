ef4b87a802aa567c7c45a30186eea855bdbf2218
Blueve <672454911@qq.com>
2015-11-18 17:40:37 +0800

    增加全局作用力发生器的测试

---
+++ UnitTest/Force/Zones/GlobalZoneTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Physics2D.Force.Zones;
+using Physics2D.Common;
+using Physics2D.Force;
+using Physics2D.Object;
+
+namespace UnitTest.Force.Zones
+{
+    [TestClass]
+    public class GlobalZoneTest
+    {
+        [TestMethod]
+        public void TestTryApplyTo()
+        {
+            var p = new Particle
+            {
+                Mass = 1
+            };
+            var zone = new GlobalZone();
+            zone.Add(new ParticleConstantForce(new Vector2D(5, 0)));
+
+            zone.TryApplyTo(p, 1 / 60.0);
+            p.Update(1 / 60.0);
+
+            Assert.AreEqual(new Vector2D(5, 0), p.Acceleration);
+        }
+    }
+}
