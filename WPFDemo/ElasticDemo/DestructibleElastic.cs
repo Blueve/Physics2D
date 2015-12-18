d98db02d25a98d14b9ea3b77508a3f9345caec00
Blueve <672454911@qq.com>
2015-12-18 19:25:45 +0800

    更改了链接破坏的条件设定

--- WPFDemo/ElasticDemo/DestructibleElastic.cs
+++ WPFDemo/ElasticDemo/DestructibleElastic.cs
@@ -5,19 +5,40 @@ using System.Collections.Generic;
 
 namespace WPFDemo.ElasticDemo
 {
-    internal class DestructibleElastic : ParticleForceGenerator
+    internal sealed class DestructibleElastic : ParticleForceGenerator
     {
+        #region 私有字段
+        /// <summary>
+        /// 所有链接
+        /// </summary>
         private readonly List<LinkedItem> _linked = new List<LinkedItem>();
 
+        /// <summary>
+        /// 弹性常量
+        /// </summary>
         private readonly double _k;
+
+        /// <summary>
+        /// 静息长度
+        /// </summary>
         private readonly double _length;
 
+        /// <summary>
+        /// 延展系数
+        /// 当链接的长度与静息长度的比值超过该数值的时发生断裂
+        /// </summary>
+        private const double LengthFactor = 1.5;
+        #endregion
+
+        #region 构造方法
         public DestructibleElastic(double k, double length)
         {
             _k = k;
             _length = length;
         }
+        #endregion
 
+        #region 实现ParticleForceGenerator
         public void Joint(Particle item)
         {
             _linked.Add(new LinkedItem
@@ -34,7 +55,8 @@ namespace WPFDemo.ElasticDemo
                 if (_linked[i].IsValid)
                 {
                     var d = particle.Position - _linked[i].Particle.Position;
-                    if (d.Length() > 1.5)
+                    // TODO: 链接破坏规则需要修正
+                    if (d.Length() > LengthFactor)
                     {
                         _linked[i].IsValid = false;
                         continue;
@@ -44,11 +66,14 @@ namespace WPFDemo.ElasticDemo
                 }
             }
         }
+        #endregion
 
+        #region 私有内部类
         private class LinkedItem
         {
             public Particle Particle;
             public bool IsValid;
         }
+        #endregion
     }
 }
